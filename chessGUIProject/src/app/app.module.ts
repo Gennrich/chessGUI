ea6741dac519fae6ac00924d40940ee70a45602c
Gennrich <103120642+Gennrich@users.noreply.github.com>
2022-05-25 13:12:26 +0200

    Game Component entfernt. Basis Webseite

--- chessGUIProject/src/app/app.module.ts
+++ chessGUIProject/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GameComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent, GameComponent]
+  bootstrap: [AppComponent, ]
 })
 export class AppModule { }
